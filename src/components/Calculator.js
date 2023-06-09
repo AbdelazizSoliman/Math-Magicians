fe748cef02577b135951696c3f930134d23fca99
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-06-09 15:06:52 +0300

    perform all initial math operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,59 @@
-import DisplayResult from './DisplayResult';
+/* eslint-disable prefer-destructuring */
+import { useState } from 'react';
+import * as math from 'mathjs';
+// import DisplayResult from './DisplayResult';
 
 function Calculator() {
+  const [result, setResult] = useState('');
+  const clickHandler = (event) => {
+    const value = event.target.value;
+    if (value === 'x') {
+      setResult(result.concat('*'));
+    } else {
+      setResult(result.concat(value));
+    }
+  };
+  const clearDisplay = () => {
+    setResult('');
+  };
+  const calculate = () => {
+    try {
+      const calculatedResult = math.evaluate(result);
+      setResult(calculatedResult.toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+  const toggleSign = () => {
+    if (result !== '') {
+      setResult((prevResult) => {
+        const num = parseFloat(prevResult);
+        return (-num).toString();
+      });
+    }
+  };
   return (
     <div className="calc">
-      <DisplayResult />
-      {/* <input type="text" value="0" id="result" /> */}
-      <input type="button" value="AC" className="button light-gray" />
-      <input type="button" value="+/-" className="button light-gray" />
-      <input type="button" value="%" className="button light-gray" />
-      <input type="button" value="/" className="button orange" />
-      <input type="button" value="7" className="button light-gray" />
-      <input type="button" value="8" className="button light-gray" />
-      <input type="button" value="9" className="button light-gray" />
-      <input type="button" value="x" className="button orange" />
-      <input type="button" value="4" className="button light-gray" />
-      <input type="button" value="5" className="button light-gray" />
-      <input type="button" value="6" className="button light-gray" />
-      <input type="button" value="-" className="button orange" />
-      <input type="button" value="1" className="button light-gray" />
-      <input type="button" value="2" className="button light-gray" />
-      <input type="button" value="3" className="button light-gray" />
-      <input type="button" value="+" className="button orange" />
-      <input type="button" value="0" className="button light-gray zero" />
-      <input type="button" value="." className="button light-gray" />
-      <input type="button" value="=" className="button orange" />
+      <input type="text" placeholder="0" value={result} id="result" />
+      <input type="button" value="AC" className="button light-gray" onClick={clearDisplay} />
+      <input type="button" value="+/-" className="button light-gray" onClick={toggleSign} />
+      <input type="button" value="%" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="/" className="button orange" onClick={clickHandler} />
+      <input type="button" value="7" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="8" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="9" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="x" className="button orange" onClick={clickHandler} />
+      <input type="button" value="4" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="5" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="6" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="-" className="button orange" onClick={clickHandler} />
+      <input type="button" value="1" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="2" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="3" className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="+" className="button orange" onClick={clickHandler} />
+      <input type="button" value="0" className="button light-gray zero" onClick={clickHandler} />
+      <input type="button" value="." className="button light-gray" onClick={clickHandler} />
+      <input type="button" value="=" className="button orange" onClick={calculate} />
     </div>
   );
 }
---
+++ src/logic/display.js
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+
+// function useResultState() {
+//   const [result, setResult] = useState('');
+
+//   const clickHandler = (event) => {
+//     setResult(result.concat(event.target.value));
+//   };
+
+//   return { result, clickHandler };
+// }
+
+// // Usage
+// // function MyComponent() {
+// //     const { result, clickHandler } = useResultState();
+
+// export default useResultState;
